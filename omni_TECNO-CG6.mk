be3791c20eb06dd3e43da276bfff9fa8bfebc7c9
infinixs5lite <kelvinchinedu97@gmail.com>
2022-10-14 09:00:32 +0100

    Update omni_TECNO-CG6.mk

--- omni_TECNO-CG6.mk
+++ omni_TECNO-CG6.mk
@@ -7,16 +7,17 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from TECNO-CG6 device
-$(call inherit-product, device/tecno/TECNO-CG6/device.mk)
+# Inherit from CG6 device
+$(call inherit-product, device/tecno/CG6/device.mk)
 
-PRODUCT_DEVICE := TECNO-CG6
-PRODUCT_NAME := omni_TECNO-CG6
+PRODUCT_DEVICE := CG6
+PRODUCT_NAME := twrp-CG6
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO CG6
 PRODUCT_MANUFACTURER := tecno
